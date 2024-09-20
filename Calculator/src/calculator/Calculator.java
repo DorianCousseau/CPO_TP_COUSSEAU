334fd891f2221815eff829613c2d6e025c7fc494
doria <doria@10.3.61.231>
2024-09-20 13:02:48 +0200

    Création exercice 2 TP0. création de calculs

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dorian COUSSEAU
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("Please enter the operator\n1) add\n2) substract\n3) multiply\n4) divide\n5) modulo");
+    int nb;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("\n Entrer le nombre :");
+    nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+    int operateur;
+    operateur = nb;
+    int val1;
+    //val=5;
+    System.out.println("\n Entrer le nombre :");
+    val1=sc.nextInt(); // On demande a sc de donner le prochain entier
+    int operande1;
+    operande1=val1;
+    int val2;
+    System.out.println("\n Entrer le nombre :");
+    val2=sc.nextInt(); // On demande a sc de donner le prochain entier
+    int operande2;
+    operande2=val2;
+    if (nb==1);{
+    System.out.println(val1+val2);}
+    else if(nb==2);{
+    System.out.println(val1-val2);}
+    else if(nb==3)
+    }
+    
+    }
+    
+}
