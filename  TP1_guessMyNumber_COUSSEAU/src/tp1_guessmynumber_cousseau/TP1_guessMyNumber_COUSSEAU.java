0e6278d08866032967085eee91b7cf42e84affa2
doria <doria@10.3.61.231>
2024-09-27 15:29:28 +0200

    TP1 exercices2 debut de l'exo

---
+++  TP1_guessMyNumber_COUSSEAU/src/tp1_guessmynumber_cousseau/TP1_guessMyNumber_COUSSEAU.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_cousseau;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Dorian COUSSEAU
+ */
+public class TP1_guessMyNumber_COUSSEAU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    
+Random generateurAleat = new Random();
+int n = generateurAleat.nextInt(100);
+
+    }
+    }
+}
