d75b64a4b07c96d754832b1202c717f59c86e9cf
doria <doria@10.3.61.231>
2024-10-11 12:58:38 +0200

    TP3 debut utilisation et creation d'un objet, plus defintion d'étiquettes

---
+++ TP2_Bieres_Cousseau/src/tp2_bieres_cousseau/BouteilleBiere.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_cousseau;
+
+/**
+ *
+ * @author doria
+ */
+public class BouteilleBiere {
+String nom;
+float degreAlcool;
+String brasserie;
+Boolean ouverte;
+
+public void lireEtiquette() {
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+ " degres) \nBrasserie : " + brasserie ) ;
+}
+public BouteilleBiere(String unNom, float unDegre, String uneBrasserie){
+ nom = unNom;
+ degreAlcool = unDegre;
+ brasserie = uneBrasserie;
+ ouverte = false;
+}
+ public boolean decapsuler() {
+    if (ouverte) {
+        System.out.println("Erreur : la bière " + nom + " est déjà ouverte.");
+        return false;
+    }
+    ouverte = true;
+    System.out.println("La bière " + nom + " est maintenant ouverte.");
+    return true;
+    
+}
+}
+
