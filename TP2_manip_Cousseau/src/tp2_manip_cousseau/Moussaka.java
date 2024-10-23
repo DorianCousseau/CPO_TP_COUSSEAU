c66b36ff2b2d10e40f197f4782dd5526b6d8f10c
doria <doria@10.3.61.231>
2024-10-23 14:38:01 +0200

    tp2 manip

---
+++ TP2_manip_Cousseau/src/tp2_manip_cousseau/Moussaka.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_cousseau;
+
+/**
+ *
+ * @author doria
+ */
+public class Moussaka {
+    int nbCalories;
+
+    // Constructeur qui initialise l'attribut nbCalories
+    public Moussaka(int calories) {
+        this.nbCalories = calories;
+    }
+}
---
+++ TP2_manip_Cousseau/src/tp2_manip_cousseau/Tartiflette.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_cousseau;
+
+/**
+ *
+ * @author doria
+ */
+public class Tartiflette {
+    int nbCalories;
+
+    // Constructeur qui initialise l'attribut nbCalories
+    public Tartiflette(int calories) {
+        this.nbCalories = calories;
+    }
+}
+
