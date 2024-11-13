5f23e632a52288603fafd6e65b0977f31409140d
doria <doria@10.3.61.231>
2024-11-13 12:58:19 +0100

    tp2 relation

---
+++ TP3_Heroic_Fantasy_Cousseau/src/Arme.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author doria
+ */
+public abstract class Arme {
+
+    private String nom;
+    private int niveauAttaque;
+
+    public Arme(String nom, int niveauAttaque) {
+        this.nom = nom;
+        if (niveauAttaque <= 100) {
+            this.niveauAttaque = niveauAttaque;
+        } else {
+            throw new IllegalArgumentException("Le niveau d'attaque doit être <= 100");
+        }
+    }
+
+    public int getNiveauAttaque() {
+        return niveauAttaque;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Arme: " + nom + ", Niveau d'attaque: " + niveauAttaque;
+    }
+}
+
+
+
